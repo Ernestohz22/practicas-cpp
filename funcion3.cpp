3489ffc23b2fe33b52cc4e576fbc99af7789b2e4
Ernestohz22 <netohs81@gmail.com>
2025-09-05 18:34:58 -0600

    Add files via upload
    Subiendo Hola Mundo y funciones básicas en C++ para revisión

---
+++ funcion3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int msn() {
+    cout << "Hola";
+    return 0;
+}
+
+int main() {
+    msn();
+    return 0;
+}
---
+++ funcion4y5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a = 8;
+int b = 2;
+int r = 0;
+
+int main() {
+    cout << "valor=r: " << r << endl;
+    r = a + b;
+    cout << "valor=r: " << r << endl;
+    return 0;
+}
