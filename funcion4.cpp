c50c8246822833c186983a17035e33b9e11daa49
Ernesto Hernández <netohs81@gmail.com>
2025-09-07 00:42:57 -0600

    Update and rename funcion4y5.cpp to funcion4.cpp
    Función editada

---
+++ funcion4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a = 8;
+int b = 2;
+int r = 1;
+
+int main() {
+    cout << "valor=r: " << r;
+    r = a + b;
+    cout << "valor=r: " << r;
+    return 0;
+}
